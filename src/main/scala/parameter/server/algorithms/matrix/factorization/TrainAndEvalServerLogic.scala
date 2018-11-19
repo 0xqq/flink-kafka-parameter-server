19c6e192af330d18432d42a498a71418464d8751
Molnár András J <modras@ilab.sztaki.hu>
2018-11-19 16:21:03 +0100

    Kafka-Redis version of server logic copied to this branch for reference.

--- src/main/scala/parameter/server/algorithms/matrix/factorization/TrainAndEvalServerLogic.scala
+++ src/main/scala/parameter/server/algorithms/matrix/factorization/TrainAndEvalServerLogic.scala
@@ -8,24 +8,61 @@ import parameter.server.logic.server.{AsynchronousServerLogic, SynchronousServer
 import parameter.server.utils.Types.ItemId
 import parameter.server.utils.{Types, Vector}
 
-class TrainAndEvalServerLogic(_init: Int => Vector, _update: (Vector, Vector) => Vector)
-  extends SynchronousServerLogic[Long, Int, Vector] {
+import com.redis._
+
+class TrainAndEvalServerLogic(_init: Int => Vector, _update: (Vector, Vector) => Vector)  extends SynchronousServerLogic[Long, Int, Vector] {
+  // WARNING !! NOT USED: - only kept because class must implement as abstract field/method - can be deleted...
   override lazy val model: ValueState[Vector] = getRuntimeContext.getState(
     new ValueStateDescriptor[Vector]("shared parameters", classOf[Vector]))
+
+  @transient lazy val r = new RedisClient("localhost", 6379)
 
   @transient lazy val init: Int => Vector = _init
   @transient lazy val update: (Vector, Vector) => Vector = _update
 
   override def onPullReceive(pull: Messages.Pull[Long, Int, Vector],
                              out: Collector[Either[Types.ParameterServerOutput, Messages.Message[Int, Long, Vector]]]): Unit = {
-    out.collect(Right(PullAnswer(pull.dest, pull.src, getOrElseUpdate(init(pull.dest)))))
+    //// original code with state:
+    //out.collect(Right(PullAnswer(pull.dest, pull.src, getOrElseUpdate(init(pull.dest)))))
+
+    //DEBUG:
+    //println("Querying user vector " + pull.dest)
+
+    val valueMapOpt =  r.hgetall1(pull.dest)
+
+    val valueVect: Vector = valueMapOpt match {
+      case Some(valueMap) =>
+        var valueVectTmp = Vector(valueMap.keys.size)
+        for (i <- valueMap.keys) {
+          valueVectTmp.value(i.toInt) = valueMap.get(i).get.toDouble
+        }
+        //DEBUG:
+        //println("Queried user vector " + pull.dest + ": " + valueVectTmp.toString, " = ", valueMap.toString())
+
+        valueVectTmp
+      case None =>
+        val valueVectTmp = init(pull.dest)
+
+        //DEBUG:
+        //print("Initialized user vector " + pull.dest + ": ")
+        //valueVectTmp.value.zipWithIndex.map(e => (e._2,e._1)).foreach(e => print(e._1," -> ", e._2))
+
+        r.hmset(pull.dest, valueVectTmp.value.zipWithIndex.map(e => (e._2,e._1)))
+        valueVectTmp
+    }
+    out.collect(Right(PullAnswer(pull.dest, pull.src, valueVect)))
   }
 
   override def onPushReceive(push: Messages.Push[Long, Int, Vector],
                              out: Collector[Either[Types.ParameterServerOutput, Messages.Message[Int, Long, Vector]]]): Unit = {
-    val oldParam = model.value()
+    //// original code with state:
+    //val oldParam = model.value()
+    //model.update(update(oldParam, push.msg))
 
-    model.update(update(oldParam, push.msg))
+    // WARNING: !! WE DON'T USE THE update FUNCTION SINCE THE INCREMENT IS DONE BY THE DB
+    for (i <- push.msg.value.indices) {
+      r.hincrbyfloat(push.dest, i, push.msg.value(i).toFloat)
+    }
   }
 }
 
